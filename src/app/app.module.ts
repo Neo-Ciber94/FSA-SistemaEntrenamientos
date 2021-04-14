d51026a6488daa743aac2661b769b878e4da7dff
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-04-14 18:36:15 -0400

    Header es ahora responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/layout/footer.component';
 import { HeaderComponent } from './shared/layout/header.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from './layout/footer.component';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HomeComponent,
-    NotFoundComponent
-  ],
+  declarations: [],
   imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
