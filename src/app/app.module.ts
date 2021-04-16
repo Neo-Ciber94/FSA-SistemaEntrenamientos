aadcc8b27e0bfcae771fb47584e1513cb29366d1
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-04-16 15:16:59 -0400

    Se agregó filtro a la lista de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './views/shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CourseModule } from './views/course/course.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    CourseModule,
+    AppRoutingModule,
+    NgbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/views/course/course.module.ts
+++ src/app/views/course/course.module.ts
@@ -5,9 +5,10 @@ import { CourseRoutingModule } from './course-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { CourseCardComponent } from './course-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [CoursesComponent, CourseCardComponent],
-  imports: [ComponentsModule, CommonModule, CourseRoutingModule],
+  imports: [ComponentsModule, CommonModule, CourseRoutingModule, NgbModule],
 })
 export class CourseModule {}
