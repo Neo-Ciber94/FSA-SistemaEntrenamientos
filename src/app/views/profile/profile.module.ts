8ced7f0e681ea8219218bc211a6596828ef81127
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-04-17 14:19:43 -0400

    Se agregó el rol al profile

--- src/app/views/profile/profile.module.ts
+++ src/app/views/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { EditProfileComponent } from './edit-profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [ProfileComponent, EditProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, NgbModule],
 })
 export class ProfileModule {}
