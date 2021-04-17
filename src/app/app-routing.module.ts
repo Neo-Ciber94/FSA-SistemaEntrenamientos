15f8a44828d8fe0e98f222a7a6bea221a7c4b3ee
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-04-17 13:55:55 -0400

    Se creó modulo profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './views/shared/not-found/not-found.component';
-import { EditProfileComponent } from './views/user/edit-profile/edit-profile.component';
-import { ProfileComponent } from './views/user/profile/profile.component';
 
 const routes: Routes = [
   {
@@ -11,23 +9,14 @@ const routes: Routes = [
       import('./views/home/home.module').then((m) => m.HomeModule),
   },
   {
-    path: 'users',
-    // TODO: Check is login
+    path: 'profile',
     loadChildren: () =>
-      import('./views/user/user.module').then((m) => m.UserModule),
+      import('./views/profile/profile.module').then((m) => m.ProfileModule),
   },
   {
     path: 'courses',
     loadChildren: () =>
       import('./views/course/course.module').then((m) => m.CourseModule),
-  },
-  {
-    path: 'profile',
-    component: ProfileComponent,
-  },
-  {
-    path: 'profile/edit',
-    component: EditProfileComponent,
   },
   {
     path: '**',
---
+++ src/app/views/profile/profile.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileComponent } from './profile.component';
+import { EditProfileComponent } from './edit-profile.component';
+
+@NgModule({
+  declarations: [ProfileComponent, EditProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule],
+})
+export class ProfileModule {}
