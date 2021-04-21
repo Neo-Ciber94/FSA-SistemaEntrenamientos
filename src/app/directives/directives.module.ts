c8a407c7536a0e02914de0dcff45c052d39aae03
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-04-21 13:10:05 -0400

    Se cambió el boton de sorting de course por el tipo de course

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RepeatDirective } from './repeat.directive';
-
 @NgModule({
   declarations: [RepeatDirective],
   imports: [CommonModule],
